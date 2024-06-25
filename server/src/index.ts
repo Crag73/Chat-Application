72a286b3e443509511868fdbbbb9e9018cceb5cd
Divyam <divyambajal73@gmail.com>
2024-06-26 02:18:07 +0530

    Project

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "http://localhost:5174"
     ]
   }
 });
